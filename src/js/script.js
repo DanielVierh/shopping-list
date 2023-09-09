6dab36034597cca7f068adf8318589f22c29180f
DanielVierh <DanielVierh@users.noreply.github.com>
2023-09-09 17:31:25 +0200

    show currency

--- src/js/script.js
+++ src/js/script.js
@@ -220,7 +220,7 @@ function render_Product_list(arr) {
         let prod_container = document.createElement('div');
         let amount_label = document.createElement('p');
         let edit_button = document.createElement('div');
-        amount_label.innerHTML = product.product_price;
+        amount_label.innerHTML = `${product.product_price}€`;
         amount_label.classList.add('amount-label');
         prod_container.innerHTML = product.product_name;
         prod_container.classList.add('product');
